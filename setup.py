9dfb33f4364bb2515974e9a34551eb0a1fc4bd4a
dgelessus <dgelessus@users.noreply.github.com>
2019-07-13 11:35:30 +0200

    Convert setup.py setup(...) options to declarative setup.cfg

--- setup.py
+++ setup.py
@@ -2,30 +2,4 @@
 
 import setuptools
 
-with open("README.rst", "r", encoding="utf-8") as f:
-	long_description = f.read()
-
-setuptools.setup(
-	name="rsrcfork",
-	version="1.1.2",
-	description="A pure Python library for reading old Macintosh resource manager data",
-	long_description=long_description,
-	url="https://github.com/dgelessus/python-rsrcfork",
-	author="dgelessus",
-	license="MIT",
-	classifiers=[
-		"Development Status :: 4 - Beta",
-		"Intended Audience :: Developers",
-		"Topic :: Software Development :: Libraries :: Python Modules",
-		"Topic :: Utilities",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
-		"Programming Language :: Python",
-		"Programming Language :: Python :: 3",
-		"Programming Language :: Python :: 3 :: Only",
-		"Programming Language :: Python :: 3.6",
-	],
-	keywords="rsrc fork resource manager macintosh mac macos",
-	python_requires=">=3.6",
-	packages=["rsrcfork"],
-)
+setuptools.setup()
